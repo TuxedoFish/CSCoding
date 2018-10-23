9fc9f070ce81f5ca7e1ae2ccf133864edd2dcc52
TuxedoFish <harryliversedge@gmail.com>
2018-10-23 08:17:59 +0100

    values can be up to 2^31 - 1 not 2^16 -1 as question stated

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -9,22 +9,21 @@ import answers.TwoBitSplit;
 
 public class Question1 {
 	//Constants that define both the problem and the way I solve it
-	public static int NUMBER_OF_BITS = 16;
+	//Ignore 1st bit as it -ve and +ve sign
+	public static int NUMBER_OF_BITS = 31;
 	public static int BITS_OBSERVED = 1;
 	
 	public static int bestMergedPortfolio(int[] portfolios) {
-		System.out.println(portfolios.length);
 		Arrays.sort(portfolios);
-		System.out.println("MIN : " + portfolios[0] + " : MAX : " + portfolios[portfolios.length-1]);
 		//Edge case: size 1 or 0
 		if(portfolios.length<=1) {
 			return 0;
 		}
 
 		int maxEvalBruteForce = 0;
-		for(int i=1; i<portfolios.length; i++) {
+		for(int i=portfolios.length-1; i>=0; i--) {
 			for(int j=0; j<i; j++) {
-				int value = (portfolios[i]&65535)^(portfolios[j]&65535);
+				int value = (portfolios[i]&2147483647)^(portfolios[j]&2147483647);
 				if(value > maxEvalBruteForce) {
 					maxEvalBruteForce = value;
 				}
